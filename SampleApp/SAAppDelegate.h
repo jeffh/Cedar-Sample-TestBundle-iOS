4810c348897cf68ae3e7d41100feb2d25059ad99
Jeff Hui <jeff@jeffhui.net>
2014-02-06 00:58:36 -0800

    First. Sample iOS App with Cedar Test Bundles

---
+++ SampleApp/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  SampleApp
+//
+//  Created by Jeff Hui on 2/6/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Jeff Hui on 2/6/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+    }
+}
